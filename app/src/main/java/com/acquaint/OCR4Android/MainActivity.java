518d9bb84920d5a421e7eb13363425bbbbc03189
DivyaPrajapati <divya@acquaintsoft.com>
2018-08-08 17:36:40 +0530

    initial coomit

---
+++ app/src/main/java/com/acquaint/OCR4Android/MainActivity.java
@@ -0,0 +1,217 @@
+package com.acquaint.OCR4Android;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import org.komamitsu.text_recognisition_sample.R;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_GALLERY = 0;
+    private static final int REQUEST_CAMERA = 1;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int MY_PERMISSION_WRITE_EXTERNAL_STORAGE = 100;
+    private static final int MY_PERMISSION_ACCESS_CAMERA = 101;
+
+    private Uri imageUri;
+    private TextView detectedTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSION_WRITE_EXTERNAL_STORAGE);
+            }
+        }
+
+        findViewById(R.id.choose_from_gallery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_GALLERY);
+            }
+        });
+
+        findViewById(R.id.take_a_photo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                MY_PERMISSION_ACCESS_CAMERA);
+                    } else {
+                        openCamera();
+                    }
+                }
+            }
+        });
+
+        detectedTextView = findViewById(R.id.detected_text);
+        detectedTextView.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    private void openCamera() {
+        String filename = System.currentTimeMillis() + ".jpg";
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, filename);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_WRITE_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! do the
+                    // calendar task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+            case MY_PERMISSION_ACCESS_CAMERA:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+        }
+    }
+
+    private void inspectFromBitmap(Bitmap bitmap) {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+        try {
+            if (!textRecognizer.isOperational()) {
+                new AlertDialog.
+                        Builder(this).
+                        setMessage("Text recognizer could not be set up on your device").show();
+                return;
+            }
+
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> origTextBlocks = textRecognizer.detect(frame);
+            List<TextBlock> textBlocks = new ArrayList<>();
+            for (int i = 0; i < origTextBlocks.size(); i++) {
+                TextBlock textBlock = origTextBlocks.valueAt(i);
+                textBlocks.add(textBlock);
+            }
+            Collections.sort(textBlocks, new Comparator<TextBlock>() {
+                @Override
+                public int compare(TextBlock o1, TextBlock o2) {
+                    int diffOfTops = o1.getBoundingBox().top - o2.getBoundingBox().top;
+                    int diffOfLefts = o1.getBoundingBox().left - o2.getBoundingBox().left;
+                    if (diffOfTops != 0) {
+                        return diffOfTops;
+                    }
+                    return diffOfLefts;
+                }
+            });
+
+            StringBuilder detectedText = new StringBuilder();
+            for (TextBlock textBlock : textBlocks) {
+                if (textBlock != null && textBlock.getValue() != null) {
+                    detectedText.append(textBlock.getValue());
+                    detectedText.append("\n");
+                }
+            }
+
+            detectedTextView.setText(detectedText);
+        } finally {
+            textRecognizer.release();
+        }
+    }
+
+    private void inspect(Uri uri) {
+        InputStream is = null;
+        Bitmap bitmap = null;
+        try {
+            is = getContentResolver().openInputStream(uri);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            options.inSampleSize = 2;
+            options.inScreenDensity = DisplayMetrics.DENSITY_LOW;
+            bitmap = BitmapFactory.decodeStream(is, null, options);
+            inspectFromBitmap(bitmap);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "Failed to find the file: " + uri, e);
+        } finally {
+            if (bitmap != null) {
+                bitmap.recycle();
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    Log.w(TAG, "Failed to close InputStream", e);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_GALLERY:
+                if (resultCode == RESULT_OK) {
+                    inspect(data.getData());
+                }
+                break;
+            case REQUEST_CAMERA:
+                if (resultCode == RESULT_OK) {
+                    if (imageUri != null) {
+                        inspect(imageUri);
+                    }
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
+    }
+}
